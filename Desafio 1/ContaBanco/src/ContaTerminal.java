707bc49d256b12affa902d3b63c31fd1b0f75ce7
jaquedb <jaqueline_diasbatista@hotmail.com>
2025-03-08 21:40:02 -0300

    conta

--- Desafio 1/ContaBanco/src/ContaTerminal.java
+++ Desafio 1/ContaBanco/src/ContaTerminal.java
@@ -6,9 +6,15 @@ public class ContaTerminal {
 
         double saldo = 200.00;
 
+        /*Obs. Na variável "numero" precisei fazer a conversão de nextLine para int
+        pois ao colocar um Scanner do tipo inteiro (numero) antes de um Scanner 
+        do tipo String (agencia), o programa apresenta um erro (pula uma etapa). 
+        Para evitar esse erro, usar sempre nextLine em tudo e converter depois. 
+        ou inserir sc.nextLine() depois de um int ou double que for seguido de uma String
+        */
+
         System.out.println("Informe o número da conta: ");
-        int numero = sc.nextInt();
-        sc.nextLine();
+        int numero = Integer.parseInt(sc.nextLine());
 
 
         System.out.println("Informe a agencia: ");
