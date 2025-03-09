ef39aa0f2697412dc4f8faa564b5f207585844b6
jaquedb <jaqueline_diasbatista@hotmail.com>
2025-03-08 23:57:34 -0300

    for

---
+++ Estruturas-de-repetição/EstruturaFor.java
@@ -0,0 +1,11 @@
+public class EstruturaFor {
+
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julias", "Marcos"};
+
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno no índice " + i + "é: " + alunos[i]);
+        }
+    }
+
+}
